d26955198ffb54c5d60073d09d00788020c89176
outclassstudio <mlimshs@gmail.com>
2023-04-15 21:47:49 +0900

    Feat : Set base, Main Carousel

---
+++ src/pages/Main.tsx
@@ -0,0 +1,10 @@
+import MainCarousel from "../components/MainCarousel";
+import Layout from "./Layout";
+
+export default function Main() {
+  return (
+    <Layout title="main">
+      <MainCarousel />
+    </Layout>
+  );
+}
